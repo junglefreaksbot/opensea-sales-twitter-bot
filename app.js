1e71734937181d02cb9ba63efa99b155a38d219a
junglefreaksbot <92644477+junglefreaksbot@users.noreply.github.com>
2021-10-16 21:29:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ function formatAndSendTweet(event) {
     // const imageUrl = _.get(event, ['asset', 'image_url']);
     // return tweet.tweetWithImage(tweetText, imageUrl);
 
-    return tweet.tweet(tweetText);
+    return tweet.tweetWithImage(tweetText);
 }
 
 // Poll OpenSea every 60 seconds & retrieve all sales for a given collection in either the time since the last sale OR in the last minute
