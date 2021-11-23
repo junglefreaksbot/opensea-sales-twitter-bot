a69e84db3ed6d9abed6f8c3bcce95b2f698fb081
junglefreaksbot <92644477+junglefreaksbot@users.noreply.github.com>
2021-11-23 22:38:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -20,18 +20,9 @@ function formatAndSendTweet(event) {
     const formattedUnits = ethers.utils.formatUnits(totalPrice, tokenDecimals);
     const formattedEthPrice = formattedUnits * tokenEthPrice;
     const formattedUsdPrice = formattedUnits * tokenUsdPrice;
-    
-    //const asset_id = _.get(event, ['asset', 'token_id']), _.get(event, ['asset_bundle', 'token_id']));;
 
-    //From 27 to 31 is for when the asset name don't have yet the name field compiled
-    //if(assetName != null){
-    //    const tweetText = `${assetName} BOUGHT FOR ${formattedEthPrice}${ethers.constants.EtherSymbol} ($${Number(formattedUsdPrice).toFixed(2)}) #JUNGLEFREAKS ${openseaLink}`;
-    //}else{
-    //     const tweetText = `JUNGLE FREAK ${asset_id} BOUGHT FOR ${formattedEthPrice}${ethers.constants.EtherSymbol} ($${Number(formattedUsdPrice).toFixed(2)}) #JUNGLEFREAKS ${openseaLink}`;
-    //}
-    
-    const tweetText = `${assetName} BOUGHT FOR ${formattedEthPrice}${ethers.constants.EtherSymbol} ($${Number(formattedUsdPrice).toFixed(2)}) #JUNGLEFREAKS ${openseaLink}`;
-    
+    const tweetText = `${assetName} bought for ${formattedEthPrice}${ethers.constants.EtherSymbol} ($${Number(formattedUsdPrice).toFixed(2)}) #NFT ${openseaLink}`;
+
     console.log(tweetText);
 
     // OPTIONAL PREFERENCE - don't tweet out sales below X ETH (default is 1 ETH - change to what you prefer)
@@ -41,11 +32,10 @@ function formatAndSendTweet(event) {
     // }
 
     // OPTIONAL PREFERENCE - if you want the tweet to include an attached image instead of just text
-    const imageUrl = _.get(event, ['asset', 'image_url']);
-    console.log(imageUrl);
-    
-    return tweet.tweetWithImage(tweetText, imageUrl);
-      //  return tweet.tweet(tweetText);
+    // const imageUrl = _.get(event, ['asset', 'image_url']);
+    // return tweet.tweetWithImage(tweetText, imageUrl);
+
+    return tweet.tweet(tweetText);
 }
 
 // Poll OpenSea every 60 seconds & retrieve all sales for a given collection in either the time since the last sale OR in the last minute
@@ -55,6 +45,9 @@ setInterval(() => {
     console.log(`Last sale (in seconds since Unix epoch): ${cache.get('lastSaleTime', null)}`);
 
     axios.get('https://api.opensea.io/api/v1/events', {
+        headers: {
+            'X-API-KEY': process.env.X_API_KEY
+        },
         params: {
             collection_slug: process.env.OPENSEA_COLLECTION_SLUG,
             event_type: 'successful',
